1e84d64b908333e816aef9db4605a41ee8cfa389
brandonladen <brandonladen486@gmail.com>
2024-10-08 08:17:39 +0300

    Added tailwind configuration

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,8 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./client/src/**/*.{html,js}'],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
+  content: ['./client/src/**/*.{html,js}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
